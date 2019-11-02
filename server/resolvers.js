51571d0475898bfd047ab0ec65dbdd2f7aab02db
anthonygharvey <anthony@anthonygharvey.com>
2019-11-02 17:23:04 -0400

    Add Apollo Server to existing app

---
+++ server/resolvers.js
@@ -0,0 +1,6 @@
+const Query = {
+  // resolver function for the query field
+  greeting: () => 'Hello World!'
+};
+
+module.exports = { Query };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs'); // file system module (one of the built-in nood APIs)
+const { ApolloServer, gql } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
@@ -9,20 +11,34 @@ const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false
+  })
+);
+
+// instead of calling gql with a template literal, can call it with a regular function and use fs
+const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf8' }));
+const resolvers = require('./resolvers');
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+// plug ApolloServer into the existing Express application
+// app is the Express app defined above
+// path is where you want to expose the graphql endpoint on the server
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+// GraphQL playground at localhost:9000/graphql (b/c that's the path we configured for the Apollo server)
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
+  const { email, password } = req.body;
+  const user = db.users.list().find(user => user.email === email);
   if (!(user && user.password === password)) {
     res.sendStatus(401);
     return;
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
 });
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
